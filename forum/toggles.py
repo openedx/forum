c4d48c4e2f19c4f77bfb681836c2113941fe88b3
Chintan Joshi <cjoshi-apphelix@2u.com>
2025-10-30 07:04:04 +0000

    add: waffle flag for the feature

--- forum/toggles.py
+++ forum/toggles.py
@@ -5,6 +5,7 @@ from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag  # type: ignor
 
 FORUM_V2_WAFFLE_FLAG_NAMESPACE = "forum_v2"
 
+DISCUSSION_WAFFLE_FLAG_NAMESPACE = "discussions"
 
 # .. toggle_name: forum_v2.enable_mysql_backend
 # .. toggle_implementation: CourseWaffleFlag
@@ -15,4 +16,14 @@ FORUM_V2_WAFFLE_FLAG_NAMESPACE = "forum_v2"
 # .. toggle_target_removal_date: 2025-06-18
 ENABLE_MYSQL_BACKEND = CourseWaffleFlag(
     f"{FORUM_V2_WAFFLE_FLAG_NAMESPACE}.enable_mysql_backend", __name__
+)
+
+# .. toggle_name: discussions.enable_ai_moderation
+# .. toggle_implementation: CourseWaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Waffle flag to enable AI moderation for discussions.
+# .. toggle_use_cases: temporary, open_edx
+# .. toggle_creation_date: 2025-10-29
+ENABLE_AI_MODERATION = CourseWaffleFlag(
+    f"{DISCUSSION_WAFFLE_FLAG_NAMESPACE}.enable_ai_moderation", __name__
 )

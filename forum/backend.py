0ceb6d51bc8c6547942bdce932dc8cfe6fac643b
salman2013 <salman.nawaz@arbisoft.com>
2026-04-22 00:51:07 +0500

    fix: quality tests

--- forum/backend.py
+++ forum/backend.py
@@ -3,6 +3,8 @@
 from forum.backends.mysql.api import MySQLBackend
 
 
-def get_backend(course_id: str | None = None) -> "type[MySQLBackend]":  # pylint: disable=unused-argument
+def get_backend(  # pylint: disable=unused-argument
+    course_id: str | None = None,
+) -> "type[MySQLBackend]":
     """Return the MySQL backend."""
     return MySQLBackend
